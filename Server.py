bb38d4a2bfd8ef0b2083565a2a5c24f3d453168c
Adam <pirog.adam@gmail.om>
2020-09-05 18:08:23 +0200

    Revert "Add packaging "
    This reverts commit 494f32c8494f3a1004ab87592ef60d79b4f7ee8d.

--- Server.py
+++ Server.py
@@ -1,8 +1,9 @@
 import socket
+from _thread import start_new_thread
+from Classes import Player
 import pickle
-import random
 import threading
-from Classes import Player, GROUND_LVL, WIDTH, HEIGHT, PLAYER_HEIGHT
+import random
 
 players = []
 players_lock = threading.Lock()
@@ -13,73 +14,64 @@ def add_new_player():
     
     with players_lock:
         new_id = len(players)
-        players.append(Player(new_id, random.randint(50, WIDTH - 50), HEIGHT - GROUND_LVL - PLAYER_HEIGHT))
+        players.append(Player(new_id, (random.randint(100, 400), random.randint(100, 400))))
         
     return new_id
 
 
-def create_packages():
-    package_list = []
-    with players_lock:
-        for player in players:
-            package_list.append(player.export_package())    
+def client_thread(conn, player):
+    global players
     
-    return package_list
-
-
-def client_thread_function(connection):
-    # message contains (client_id, info_package)
-    
-    my_id = add_new_player()
-    reply = create_packages()
-    connection.send(pickle.dumps([my_id, reply], protocol=pickle.HIGHEST_PROTOCOL))
+    new_id = add_new_player()
+    conn.send(pickle.dumps([new_id, players], protocol=pickle.HIGHEST_PROTOCOL))
+    reply = ""
     
     while True:
         try:
-            data = pickle.loads(connection.recv(2048))
-            player_id = data[0]
+            data = pickle.loads(conn.recv(2048))
+            new_id = data[0]
             with players_lock:
-                players[player_id].import_package(data[1])
+                players[new_id] = data[1]
             
             if not data:
+                print("Disconnected")
                 break
             else:
-                reply = create_packages()
+                with players_lock:
+                    reply = players
                 
-            connection.sendall(pickle.dumps(reply, protocol=pickle.HIGHEST_PROTOCOL))
+            conn.sendall(pickle.dumps(reply, protocol=pickle.HIGHEST_PROTOCOL))
         
         except Exception:
             break
         
-    print("Client disconnected")
-    players.pop(my_id)
-    connection.close()
+    print("Lost connection")
+    conn.close()
 
 
 def main():
     server = "192.168.1.25"
     port = 5555
     maxPlayers = 2
-    running = True
+    currentPlayers = 0
 
-    open_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     try:
-        open_socket.bind((server, port))
+        s.bind((server, port))
     except socket.error as e:
         print(str(e))
 
-    open_socket.listen()
+    s.listen(maxPlayers)
     print("Server started, waiting for connection")
 
-    while running:
-        if(len(players) < maxPlayers):
-            connection, address = open_socket.accept()
-            print("Connected to: ", address)
+    while True:
+        conn, addr = s.accept()
+        print("Connected to: ", addr)
 
-            client_thread = threading.Thread(target=client_thread_function, args=[connection])
-            client_thread.start()
-            
+        start_new_thread(client_thread, (conn, currentPlayers))
+        currentPlayers += 1
+
 
 if __name__ == "__main__":
     main()
